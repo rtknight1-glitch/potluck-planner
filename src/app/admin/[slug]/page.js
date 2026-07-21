99f532738a0258026fcc7299bfc35aa0bad67a55
rtknight1-glitch <rtknight1@gmail.com>
2026-07-20 17:26:02 -0700

    Update page.js

--- src/app/admin/[slug]/page.js
+++ src/app/admin/[slug]/page.js
@@ -3,6 +3,8 @@ import { supabaseAdmin } from "@/lib/supabaseAdmin";
 import AdminClient from "./AdminClient";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 async function loadEvent(slug) {
   const db = supabaseAdmin();
